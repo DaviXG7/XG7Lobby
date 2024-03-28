120bb5e13d099be256ee36302de32f4ecd797e91
DaviXG7 <davisonic2010@gmail.com>
2024-03-28 11:29:28 -0300

    a

--- src/main/java/com/xg7network/xg7lobby/Inventories/ConfigInventory.java
+++ src/main/java/com/xg7network/xg7lobby/Inventories/ConfigInventory.java
@@ -1,10 +1,10 @@
 package com.xg7network.xg7lobby.Inventories;
 
-import com.xg7network.xg7menus.API.Inventory.Menus.Others.AdvancedMenu;
+import com.xg7network.xg7menus.API.Inventory.Menus.Menu;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-public class ConfigInventory extends AdvancedMenu {
+public class ConfigInventory extends Menu {
     public ConfigInventory(FileConfiguration configuration, Player player) {
         super(configuration.getString("title"), configuration.getInt("rows") * 9, player);
     }
