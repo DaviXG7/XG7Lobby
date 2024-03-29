0cebe584bd88ae406e7389903b04b4266a8021b0
DaviXG7 <davisonic2010@gmail.com>
2024-03-29 02:56:19 -0300

    a

--- src/main/java/com/xg7network/xg7lobby/Inventories/ConfigInventory.java
+++ src/main/java/com/xg7network/xg7lobby/Inventories/ConfigInventory.java
@@ -1,11 +1,31 @@
 package com.xg7network.xg7lobby.Inventories;
 
 import com.xg7network.xg7menus.API.Inventory.Menus.Menu;
+import com.xg7network.xg7menus.API.Inventory.Menus.Others.StandardMenu;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-public class ConfigInventory extends Menu {
+import java.util.HashMap;
+import java.util.List;
+
+public class ConfigInventory extends StandardMenu {
+
+    private HashMap<Integer, List<String>> actions = new HashMap<>();
+
     public ConfigInventory(FileConfiguration configuration, Player player) {
-        super(configuration.getString("title"), configuration.getInt("rows") * 9, player);
+        super(configuration.getString("title"), configuration.getInt("rows") * 9, player, configuration.getInt("id"));
     }
+
+
+
+    public static ConfigInventory fromConfig() {
+
+
+
+    }
+
+
+
+
+
 }
