7347722c3a097163e620694a1239a29e389b694d
davis <davisonic2010@gmail.com>
2024-07-20 14:48:02 -0300

    XG7Lobby 90%

--- src/main/java/com/xg7plugins/xg7lobby/events/PacketPlayEvent.java
+++ src/main/java/com/xg7plugins/xg7lobby/events/PacketPlayEvent.java
@@ -2,7 +2,7 @@ package com.xg7plugins.xg7lobby.events;
 
 import org.bukkit.entity.Player;
 
-public abstract interface PacketPlayEvent extends Event {
+public interface PacketPlayEvent extends Event {
 
     String[] getPacketsNames();
 
